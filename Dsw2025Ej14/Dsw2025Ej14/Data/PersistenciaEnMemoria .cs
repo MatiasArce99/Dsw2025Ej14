76d8030cc08f5775b95bb0f63b17ffc0ca0af2b6
mauro <mauro@gmail.com>
2025-06-10 20:30:57 -0300

    Ejercicio-14

---
+++ Dsw2025Ej14/Dsw2025Ej14/Data/PersistenciaEnMemoria .cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using Dsw2025Ej14.Api.Domain;
+
+namespace Dsw2025Ej14.Api.Data
+{
+    public class PersistenciaEnMemoria : IPersistencia
+    {
+        private List<Product> _products;
+
+        public PersistenciaEnMemoria()
+        {
+            LoadProducts();
+        }
+
+        
+
+        private void LoadProducts()
+        {
+            var json = File.ReadAllText("..\\..\\..\\products.json");
+            _products = JsonSerializer.Deserialize<List<Product>>(json, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            }) ?? [];
+        }
+    }
+}
